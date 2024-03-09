aec7ef57f1bcbef5f231c0aa4a6acd53d5794e84
byMagg <byMag@hotmail.es>
2024-03-09 12:55:13 +0100

    Actualizar página de inicio y settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Settings() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Settings</h1>
+    <main>
+      <h1 className="text-4xl font-bold">Settings</h1>
+      <Button>Save</Button>
     </main>
   )
 }
