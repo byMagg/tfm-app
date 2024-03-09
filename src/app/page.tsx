8e4963d977603cd59a65bf55504682252fd6c1e3
byMagg <byMag@hotmail.es>
2024-03-09 13:51:19 +0100

    Ejemplo de ViewTransition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
+import { flushSync } from 'react-dom'
 
 export default function Home() {
+  const [listado, setListado] = useState(['HOLA'])
+
+  const handleViewTransition = () => {
+    // @ts-ignore
+    document.startViewTransition(() => {
+      flushSync(() => {
+        setListado([...listado, 'CHAU'])
+      })
+    })
+  }
+
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>Home</h1>
@@ -14,6 +30,16 @@ export default function Home() {
           height={200}
         />
       </Link>
+
+      <Button onClick={handleViewTransition}>Añadir elemento</Button>
+
+      <section className="grid grid-cols-6">
+        {listado.map((item, index) => (
+          <div className="bg-gray-200 p-4 m-4 rounded-md" key={index}>
+            {item}
+          </div>
+        ))}
+      </section>
     </main>
   )
 }
