2a27010bdced7d95434b90f0956dd5ad4707cf02
byMagg <byMag@hotmail.es>
2024-03-14 22:32:22 +0100

    Add types TrafficCrash

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -4,3 +4,12 @@ export type Payment = {
   status: "pending" | "processing" | "success" | "failed";
   email: string;
 };
+
+export type TrafficCrash = {
+  CRASH_RECORD_ID: string;
+  CRASH_DATE: string;
+  WEATHER_CONDITION: string;
+  ROAD_DEFECT: string;
+  LOCATION: string;
+  STREET_NAME: string;
+};
