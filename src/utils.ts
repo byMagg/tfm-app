9fc48fe4c147c3f6133b8c1d73a04c540fffb95d
byMagg <byMag@hotmail.es>
2024-05-10 12:40:06 +0200

    feat: Add WikiData API retrieving

--- src/utils.ts
+++ src/utils.ts
@@ -19,6 +19,35 @@ export async function fetchAPI(query: string) {
   }
 }
 
+export async function fetchWikidata(endpoint: string) {
+  const token =
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI4NDhiZDhmNDY4N2VkNzI4MTk5ZDA2NWQzZmQ0MjQ2ZSIsImp0aSI6IjE4NzBlNDM1YjQ1ZjUyNDYwYjUwM2Q2YmY3NWM4OGJjM2IwNDhlNTI4N2RlODUzYTI4MDZkNjJkZjU5YTExMzAxYTU5M2MwMzNiZDI3NDI2IiwiaWF0IjoxNzE1MzM0MjE5LjI5MTQxNSwibmJmIjoxNzE1MzM0MjE5LjI5MTQyLCJleHAiOjMzMjcyMjQzMDE5LjI4Mzk5LCJzdWIiOiI3NTYwNjYxMSIsImlzcyI6Imh0dHBzOi8vbWV0YS53aWtpbWVkaWEub3JnIiwicmF0ZWxpbWl0Ijp7InJlcXVlc3RzX3Blcl91bml0Ijo1MDAwLCJ1bml0IjoiSE9VUiJ9LCJzY29wZXMiOlsiYmFzaWMiLCJoaWdodm9sdW1lIl19.OXEYaFUjgfmp_4upSlV-gsHvDwZZQ41zWa-q77MxImkPRw66oTbHcIkEJb1U-SEOz57j8rZTX7bp3YYsGEGWsFJS-N8XVpO68hogorRG0151duL2EDvcwG2ouglqEvO2lu8NxHGf2F41ORrg5tQvokiZZVTsCvJzxvu95IaNlm6x_g7K_cCPIyaA7YlaXeKriMoaTAZpalC5R9bJodekXUXO64B31NRvmtV8GV3FyGRAxs31lZJWgWAOu-ty9VOrEh31537qLPuYwDcWBWW0xeM2qldKSbbGyd6rBgN1AaaSf1knM81I7vD3ZRWTlMmNQ5ROBwvZps9eByqSiHxcAb9FIV9NFL6IJAE8mNv_E-K7oFc6PXQblQHSwXhXl2KiBHpLcQUOExMOQDac1kBa6OgKVaPdQDCC3rnIAbUEw3z13aBSIJ4zE1eMvMcoxzyubkmuXoDBIn3I1oLspdvvL_vXpKJ4j36VAaq-s1mAJ8UP4HlRJFXsSlEGgcQPS4N-KknDMYfWKvxC6dJrjSyV5nWXs8xGitdHUtBVW1plXOUNPDpYwegXdBCp7xQFmAWnF0fULzkxKxG8OlOtfPF79rUm7_G4Ro2iFFNw6EJyTb-Q20q_tR3_J3UOmFoJ-m3M9EG_S8u4lI2ppGi_8kzk7N13waFNv4Y0yXnsSbEMLYU";
+
+  console.log(endpoint);
+  try {
+    const response = await fetch(
+      `https://www.wikidata.org/w/rest.php/wikibase/v0${endpoint}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    if (!response.ok) throw new Error("No se ha podido obtener los datos");
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
+}
+
 export function parseDateString(date: number) {
   const dateString = date.toString();
 
