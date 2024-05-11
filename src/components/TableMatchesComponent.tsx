fab823c2349be1cd114e0be07d3261f741a9e18a
byMagg <byMag@hotmail.es>
2024-05-11 23:27:45 +0200

    feat: Update image paths to use relative directory

--- src/components/TableMatchesComponent.tsx
+++ src/components/TableMatchesComponent.tsx
@@ -30,7 +30,7 @@ const columns: ColumnDef<Match>[] = [
             style={{
               viewTransitionName: `surface-${row.original._id}`,
             }}
-            src={`/public/images/thumb-${original.surface.toLowerCase()}.webp`}
+            src={`/images/thumb-${original.surface.toLowerCase()}.webp`}
             alt="clay court"
             width="50"
             height="50"
