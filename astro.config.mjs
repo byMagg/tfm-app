66b290478599ccf9ec8ff215655ae863bb6417e9
byMagg <byMag@hotmail.es>
2024-05-11 23:12:55 +0200

    feat: Update Astro configuration and workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://bymagg.github.io",
+  base: "tfm-app",
   integrations: [
     tailwind({
       applyBaseStyles: false,
