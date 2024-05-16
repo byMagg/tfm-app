347cfa1ae3ccf8200c1c2acfcb41408a2d49119a
byMagg <byMag@hotmail.es>
2024-05-16 10:55:08 +0200

    add: node integration for docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone"
+  })
 });
