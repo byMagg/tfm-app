cdf0d44f5ac38f379d12107c878c2faa78ca0b46
byMagg <byMag@hotmail.es>
2025-04-02 11:25:04 +0200

    feat: Actualizar el formulario de calendario para manejar la fecha y hora del partido, mejorando la selección de tiempo con desplazamiento

--- src/components/CalendarForm.tsx
+++ src/components/CalendarForm.tsx
@@ -1,6 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { format } from "date-fns";
-import { CalendarIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -14,91 +13,177 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-
-import { es } from "date-fns/locale";
-
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { setMatchDate } from "@/controllers";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
-import type { LeagueMatch } from "@/types";
+import { CalendarIcon } from "lucide-react";
+import { toast } from "sonner";
+
+import { setMatchDate } from "@/controllers";
+import { es } from "date-fns/locale";
 
 const FormSchema = z.object({
-  date: z.date({
-    required_error: "A date of birth is required.",
+  time: z.date({
+    required_error: "A date and time is required.",
   }),
 });
 
 export function CalendarForm({
+  matchId,
   maxDate,
-  match,
 }: {
+  matchId?: string;
   maxDate?: Date;
-  match: LeagueMatch;
 }) {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   });
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
-    await setMatchDate({
-      matchId: match._id,
-      date: data.date.toUTCString(),
-    });
+    try {
+      await setMatchDate({ matchId, date: data.time.toUTCString() });
+
+      toast.success(
+        `Seleccionada fecha del partido: ${format(data.time, "PPPP HH:mm", {
+          locale: es,
+        })}`,
+      );
+    } catch (error) {
+      toast.error("Error al seleccionar la fecha del partido.");
+    }
+  }
+
+  function handleDateSelect(date: Date | undefined) {
+    if (date) {
+      form.setValue("time", date);
+    }
+  }
+
+  function handleTimeChange(type: "hour" | "minute", value: string) {
+    const currentDate = form.getValues("time") || new Date();
+    let newDate = new Date(currentDate);
+
+    if (type === "hour") {
+      const hour = parseInt(value, 10);
+      newDate.setHours(hour);
+    } else if (type === "minute") {
+      newDate.setMinutes(parseInt(value, 10));
+    }
+
+    form.setValue("time", newDate);
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
         <FormField
           control={form.control}
-          name="date"
+          name="time"
           render={({ field }) => (
             <FormItem className="flex flex-col">
-              <FormLabel>Fecha de partido</FormLabel>
+              <FormLabel>Fecha del partido</FormLabel>
               <Popover>
                 <PopoverTrigger asChild>
                   <FormControl>
                     <Button
                       variant={"outline"}
                       className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
+                        "w-fit pl-3 text-left font-normal gap-2",
                         !field.value && "text-muted-foreground",
                       )}
                     >
                       {field.value ? (
-                        format(field.value, "PPP")
+                        format(field.value, "dd 'de' MMMM 'a las' HH:mm", {
+                          locale: es,
+                        })
                       ) : (
-                        <span>Selecciona una fecha</span>
+                        <span>Seleccione una fecha</span>
                       )}
                       <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                     </Button>
                   </FormControl>
                 </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    locale={es}
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) => {
-                      if (date < new Date()) return true;
-                      if (maxDate && date > maxDate) return true;
-
-                      return false;
-                    }}
-                    initialFocus
-                  />
+                <PopoverContent className="w-auto p-0">
+                  <div className="sm:flex">
+                    <Calendar
+                      mode="single"
+                      locale={es}
+                      selected={field.value}
+                      onSelect={handleDateSelect}
+                      disabled={(date) =>
+                        date < new Date() || (maxDate ? date > maxDate : false)
+                      }
+                      initialFocus
+                    />
+                    <div className="flex flex-col sm:flex-row sm:h-[300px] divide-y sm:divide-y-0 sm:divide-x">
+                      <ScrollArea className="w-64 sm:w-auto">
+                        <div className="flex sm:flex-col p-2">
+                          {Array.from({ length: 24 }, (_, i) => i)
+                            .reverse()
+                            .map((hour) => (
+                              <Button
+                                key={hour}
+                                size="icon"
+                                variant={
+                                  field.value && field.value.getHours() === hour
+                                    ? "default"
+                                    : "ghost"
+                                }
+                                className="sm:w-full shrink-0 aspect-square"
+                                onClick={() =>
+                                  handleTimeChange("hour", hour.toString())
+                                }
+                              >
+                                {hour}
+                              </Button>
+                            ))}
+                        </div>
+                        <ScrollBar
+                          orientation="horizontal"
+                          className="sm:hidden"
+                        />
+                      </ScrollArea>
+                      <ScrollArea className="w-64 sm:w-auto">
+                        <div className="flex sm:flex-col p-2">
+                          {Array.from({ length: 12 }, (_, i) => i * 5).map(
+                            (minute) => (
+                              <Button
+                                key={minute}
+                                size="icon"
+                                variant={
+                                  field.value &&
+                                  field.value.getMinutes() === minute
+                                    ? "default"
+                                    : "ghost"
+                                }
+                                className="sm:w-full shrink-0 aspect-square"
+                                onClick={() =>
+                                  handleTimeChange("minute", minute.toString())
+                                }
+                              >
+                                {minute.toString().padStart(2, "0")}
+                              </Button>
+                            ),
+                          )}
+                        </div>
+                        <ScrollBar
+                          orientation="horizontal"
+                          className="sm:hidden"
+                        />
+                      </ScrollArea>
+                    </div>
+                  </div>
                 </PopoverContent>
               </Popover>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Enviar</Button>
+        <Button type="submit">Aplicar</Button>
       </form>
     </Form>
   );
