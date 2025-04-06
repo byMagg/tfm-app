572c1a8fbfa682685e6c6194d64278f6d2dc2cb3
byMagg <byMag@hotmail.es>
2025-04-06 12:52:13 +0200

    feat: Limpiar el código de verificación de sesión en la página de perfil y restaurar el manejo de jugadores en la liga

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
