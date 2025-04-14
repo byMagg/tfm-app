0e54a1856bbaffb1c02e1f0eda0f0bbe1b1e4537
byMagg <byMag@hotmail.es>
2025-04-14 17:20:48 +0200

    feat: agregar componente Spinner y mejorar manejo de carga en ProfileMatches

--- src/components/ProfileMatches.tsx
+++ src/components/ProfileMatches.tsx
@@ -1,46 +1,46 @@
 import { useCheckPlayerInLeague } from "@/hooks/useCheckPlayerInLeague";
 import type { League, LeagueMatch } from "@/types";
+import { Spinner } from "./ui/spinner";
 
 export const ProfileMatches = () => {
   const { leagues, loading, error } = useCheckPlayerInLeague();
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
   return (
-    leagues &&
-    leagues.map((league: League) => (
-      <section
-        key={league._id}
-        className="w-full flex flex-col justify-center items-center my-3"
-      >
-        <p className="text-center font-bold text-2xl">{league.name}</p>
+    <section className="w-full flex items-center justify-center min-h-48 my-3">
+      {loading && <Spinner size="lg" className="bg-black dark:bg-white" />}
+      {leagues &&
+        leagues.map((league: League) => (
+          <section
+            key={league._id}
+            className="w-full flex flex-col justify-center items-center my-3"
+          >
+            <p className="text-center font-bold text-2xl">{league.name}</p>
 
-        <article className="w-full flex flex-col justify-center items-center my-3">
-          <h2 className="text-center  text-xl">Partidos esta jornada</h2>
-          <ul>
-            {league.matches?.map((match: LeagueMatch) => (
-              <li
-                key={match._id}
-                className="my-2 bg-white p-2 rounded text-black text-center font-semibold"
-              >
-                <a
-                  href={`/league-matches/${match._id}`}
-                  style={{
-                    viewTransitionName: `match-${match._id}`,
-                  }}
-                >
-                  {match.player1.name}
-                  vs
-                  {match.player2.name}
-                </a>
-                <span>{match.score}</span>
-              </li>
-            ))}
-          </ul>
-        </article>
-      </section>
-    ))
+            <article className="w-full flex flex-col justify-center items-center my-3">
+              <h2 className="text-center  text-xl">Partidos esta jornada</h2>
+              <ul>
+                {league.matches?.map((match: LeagueMatch) => (
+                  <li
+                    key={match._id}
+                    className="my-2 bg-white p-2 rounded text-black text-center font-semibold"
+                  >
+                    <a
+                      href={`/league-matches/${match._id}`}
+                      style={{
+                        viewTransitionName: `match-${match._id}`,
+                      }}
+                    >
+                      {match.player1.name}
+                      vs
+                      {match.player2.name}
+                    </a>
+                    <span>{match.score}</span>
+                  </li>
+                ))}
+              </ul>
+            </article>
+          </section>
+        ))}
+    </section>
   );
 };
--- src/components/SignoutButton.tsx
+++ src/components/SignoutButton.tsx
@@ -9,7 +9,7 @@ export function SignoutButton() {
 
   return (
     <button
-      className="border border-0.5 border-zinc-300 text-zinc-500 dark:border-zinc-700 dark:text-zinc-500 hover:text-zinc-700 hover:border-zinc-600 font-normal dark:hover:text-zinc-100 dark:hover:border-zinc-100 transition-all px-4 py-1 rounded-md"
+      className="w-fit border border-0.5 border-zinc-300 text-zinc-500 dark:border-zinc-700 dark:text-zinc-500 hover:text-zinc-700 hover:border-zinc-600 font-normal dark:hover:text-zinc-100 dark:hover:border-zinc-100 transition-all px-4 py-1 rounded-md"
       type="button"
       onClick={signout}
     >
