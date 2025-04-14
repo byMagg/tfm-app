cc0375134edb734b3c17d48e5eb5469854011692
byMagg <byMag@hotmail.es>
2025-04-14 17:08:01 +0200

    fix: corregir manejo del estado de sesión para retornar null cuando no está definido

--- src/components/LoginFormContainer.tsx
+++ src/components/LoginFormContainer.tsx
@@ -16,11 +16,11 @@ export function LoginFormContainer() {
 
   const { isLoggedIn } = useSession();
 
-  if (isLoggedIn) {
-    return <SignoutButton />;
-  }
+  if (isLoggedIn === null) return null;
 
-  return (
+  return isLoggedIn ? (
+    <SignoutButton />
+  ) : (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Bienvenido a SlamStats
--- src/hooks/useSession.tsx
+++ src/hooks/useSession.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const useSession = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
 
   useEffect(() => {
     setIsLoggedIn(document.cookie.includes("__session"));
