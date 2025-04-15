aee4bc56e9f984d47cc93d827568ca57ad083fbc
byMagg <byMag@hotmail.es>
2025-04-15 11:56:16 +0200

    feat: actualizar contenido en HeroScroll y TracingBeam para reflejar la temática de gestión de ligas

--- src/components/HeroScroll.tsx
+++ src/components/HeroScroll.tsx
@@ -7,9 +7,10 @@ export function HeroScroll() {
         titleComponent={
           <>
             <h1 className="text-4xl font-semibold text-black dark:text-white">
-              Unleash the power of <br />
+              Crea ligas, gestiona partidos y asciende de grupo compitiendo
+              jornada a jornada. <br />
               <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                Scroll Animations
+                ¡Organiza tu tenis como un profesional!
               </span>
             </h1>
           </>
