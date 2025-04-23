4d6dd93cf68b728d3896f0aea6f4368531edb9f4
byMagg <byMag@hotmail.es>
2025-04-23 16:52:00 +0200

    feat: reemplazar LoginFormContainer por LoginForm y RegisterForm en ProfilePage, mejorando la gestión de autenticación

--- src/components/LoginFormContainer.tsx
+++ src/components/LoginFormContainer.tsx
@@ -1,44 +1,14 @@
-import { useAuth } from "@/context/AuthContext";
 import { app } from "@/lib/firebase/client";
 import { cn } from "@/lib/utils";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
-import { SignoutButton } from "./SignoutButton";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 
 const auth = getAuth(app);
 auth.setPersistence;
-
-export function LoginFormContainer() {
-  const [showRegister, setShowRegister] = useState(false);
-
-  const { user } = useAuth();
-
-  return user ? (
-    <SignoutButton />
-  ) : (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Bienvenido a SlamStats
-      </h2>
-      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Inicia sesión con tu cuenta de SlamStats
-      </p>
-      {showRegister ? <RegisterForm /> : <LoginForm />}
-      <a
-        className="cursor-pointer text-neutral-600 text-sm dark:text-neutral-300 mt-4"
-        onClick={() => setShowRegister(!showRegister)}
-      >
-        {showRegister
-          ? "Ya tienes una cuenta? Inicia sesión"
-          : "No tienes una cuenta? Registrate"}
-      </a>
-    </div>
-  );
-}
 
 const validateField = (field: string, value: string) => {
   let error;
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,11 +1,41 @@
-import { LoginFormContainer } from "@/components/LoginFormContainer";
+import { LoginForm, RegisterForm } from "@/components/LoginFormContainer";
 import { ProfileMatches } from "@/components/ProfileMatches";
+import { SignoutButton } from "@/components/SignoutButton";
+import { useAuth } from "@/context/AuthContext";
+import { useState } from "react";
 
 export const ProfilePage = () => {
+  const [showRegister, setShowRegister] = useState(false);
+
+  const { user } = useAuth();
+
   return (
     <>
-      <LoginFormContainer />
-      <ProfileMatches />
+      {user && (
+        <>
+          <SignoutButton />
+          <ProfileMatches />
+        </>
+      )}
+      {!user && (
+        <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+          <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+            Bienvenido a SlamStats
+          </h2>
+          <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            Inicia sesión con tu cuenta de SlamStats
+          </p>
+          {showRegister ? <RegisterForm /> : <LoginForm />}
+          <a
+            className="cursor-pointer text-neutral-600 text-sm dark:text-neutral-300 mt-4"
+            onClick={() => setShowRegister(!showRegister)}
+          >
+            {showRegister
+              ? "Ya tienes una cuenta? Inicia sesión"
+              : "No tienes una cuenta? Registrate"}
+          </a>
+        </div>
+      )}
     </>
   );
 };
