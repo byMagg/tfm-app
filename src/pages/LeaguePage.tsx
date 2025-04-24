a30fc46a2533c5fe9bd5ddc7ca0253505da884db
byMagg <byMag@hotmail.es>
2025-04-24 11:57:10 +0200

    feat: reestructurar la página de la liga para incluir encabezados y botones de inicio de temporada

--- src/pages/LeaguePage.tsx
+++ src/pages/LeaguePage.tsx
@@ -1,6 +1,8 @@
 import AddPlayersForm from "@/components/AddPlayersForm";
-import TableUsersComponent from "@/components/TableUsersComponent";
+import { GroupCards } from "@/components/GroupCards";
+import { HistoryMatches } from "@/components/HistoryMatches";
 import { useLeague } from "@/hooks/useLeague";
+import { Button } from "react-day-picker";
 import { useParams } from "react-router-dom";
 
 export const LeaguePage = () => {
@@ -12,8 +14,26 @@ export const LeaguePage = () => {
 
   return (
     <>
-      <AddPlayersForm leagueId={league._id} />
-      <TableUsersComponent leagueId={league._id} playerIds={league.players} />
+      <div className="flex flex-col gap-4 items-center">
+        <h1
+          className="font-bold text-2xl"
+          style={{ viewTransitionName: `league-${league._id}` }}
+        >
+          {league.name}
+        </h1>
+        {league.startedAt ? (
+          <h3 className="font-semibold text-xl text-green-200 ">EN JUEGO</h3>
+        ) : (
+          <Button>Start season</Button>
+          // <form method="POST" action={actions.startSeason}>
+          //   <input type="hidden" name="leagueId" value={league._id} />
+          // </form>
+        )}
+
+        <AddPlayersForm leagueId={id} />
+      </div>
+      <GroupCards className="my-16" leagueId={id} />
+      <HistoryMatches className="my-16" leagueId={id} />
     </>
   );
 };
