d485b13628d509aaa854b6a96e65bec7a8d9b924
byMagg <byMag@hotmail.es>
2025-04-25 11:22:49 +0200

    refactor: reorganizar el componente Layout para incluir NavigationBar y Toaster, mejorando la estructura visual

--- src/root.tsx
+++ src/root.tsx
@@ -13,6 +13,9 @@ import { AuthProvider } from "./context/AuthContext";
 import { cn } from "./lib/utils";
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
+
+  const isProfilePage = location.pathname === "/profile";
   return (
     <html className="dark" lang="en">
       <head>
@@ -23,7 +26,23 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <div className="relative flex min-h-screen h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+          <div
+            className={cn(
+              "absolute inset-0",
+              "[background-size:20px_20px]",
+              "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+              "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+            )}
+          />
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+          <div className="absolute h-full w-full items-center justify-center">
+            <NavigationBar isProfilePage={isProfilePage} />
+            <Toaster />
+            {children}
+          </div>
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -32,29 +51,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function Root() {
-  const location = useLocation();
-
-  const isProfilePage = location.pathname === "/profile";
-
   return (
     <AuthProvider>
-      <div className="relative flex min-h-screen h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
-        <div
-          className={cn(
-            "absolute inset-0",
-            "[background-size:20px_20px]",
-            "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
-            "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
-          )}
-        />
-        {/* Radial gradient for the container to give a faded look */}
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-        <div className="absolute h-full w-full items-center justify-center">
-          <NavigationBar isProfilePage={isProfilePage} />
-          <Toaster />
-          <Outlet />
-        </div>
-      </div>
+      <Outlet />
     </AuthProvider>
   );
 }
