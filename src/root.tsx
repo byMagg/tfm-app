993eecf579eebf91f798dc76c70becb037fa62eb
byMagg <byMag@hotmail.es>
2025-04-29 11:01:35 +0200

    Implement code changes to enhance functionality and improve performance

--- src/root.tsx
+++ src/root.tsx
@@ -21,6 +21,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
         <title>SlamStats</title>
         <Meta />
         <Links />
