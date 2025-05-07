83de68bc7409f726d0c6d583727dd0e1270fa6b3
byMagg <byMag@hotmail.es>
2025-05-07 17:17:31 +0200

    feat: agregar configuración de Playwright y pruebas de inicio de sesión

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,11 @@
+import { test } from "@playwright/test";
+
+test("check login", async ({ page }) => {
+  await page.goto("http://localhost:4321/");
+  await page.getByRole("textbox", { name: "Email" }).click();
+  await page.getByRole("textbox", { name: "Email" }).fill("pepe@gmail.com");
+  await page.getByRole("textbox", { name: "Contraseña" }).click();
+  await page.getByRole("textbox", { name: "Contraseña" }).fill("password");
+  await page.getByRole("button", { name: "Iniciar sesión →" }).click();
+  await page.getByRole("button", { name: "Cerrar sesión" }).click();
+});
